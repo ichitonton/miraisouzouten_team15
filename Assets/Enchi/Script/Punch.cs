220992a33c23f415d111020ae89c63284843e1dc
EnchiIchito <ichitonton@gmail.com>
2025-11-07 17:47:08 +0900

    これでなおってくれや

--- Assets/Enchi/Script/Punch.cs
+++ Assets/Enchi/Script/Punch.cs
@@ -2,10 +2,20 @@ using UnityEngine;
 
 public class Punch : MonoBehaviour
 {
+    float _punchForce = 10.0f;
+    float _stunTime = 1.0f;
+    int _punchDamage = 10;
+    MovePlayerKey _player;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _player = transform.parent.GetComponent<MovePlayerKey>();
+        if (_player != null)
+        {
+            _punchForce = _player.GetPunchForce();
+            _stunTime = _player.GetStunTime();
+            _punchDamage = _player.GetPunchDamage();
+        }
     }
 
     void OnTriggerEnter(Collider other)
@@ -16,10 +26,10 @@ public class Punch : MonoBehaviour
 
             if (other.GetComponent<MovePlayerKey>() != null)
             {
-                other.GetComponent<MovePlayerKey>().ToGetPunch(transform.parent.GetComponent<MovePlayerKey>().GetPunchDamage());
+                other.GetComponent<MovePlayerKey>().ToGetPunch(_punchDamage, _stunTime);
             }
 
-            other.GetComponent<Rigidbody>().AddForce((this.transform.forward + Vector3.up * 0.1f) * 10.0f, ForceMode.Impulse);
+            other.GetComponent<Rigidbody>().AddForce((this.transform.forward + Vector3.up * 0.1f) * _punchForce, ForceMode.Impulse);
         }
     }
 }
--- Assets/Enchi/Script/playerSpawner.cs
+++ Assets/Enchi/Script/playerSpawner.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Net.NetworkInformation;
 using UnityEngine;
 using UnityEngine.Profiling;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class playerSpawner : MonoBehaviour
 {
@@ -16,37 +17,53 @@ public class playerSpawner : MonoBehaviour
     [SerializeField] float _lowerDamper = 150f;
     [SerializeField] float halfHeight = 0.5f; // オブジェクトの半分の高さ
 
+    private GameObject _playerA;
+    private GameObject _playerB;
     private List<GameObject> _joints;
+
+    private float _magicNumber = 0.5f;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Spawn();
-
-
+        PlayerSpawn();
     }
-    void Spawn()
+
+    void PlayerSpawn()
+    {
+        _playerA = Instantiate(_player1, this.transform.position + new Vector3(0, -1, 0), transform.rotation);
+        _playerB = Instantiate(_player2, this.transform.position + new Vector3(0.01f * (_jointCount + 1), -1, 0), transform.rotation);
+    }
+
+    private void OnGUI()
+    {
+        //ホストとして入る
+        if (GUI.Button(new Rect(100, Screen.height  - 30, 100, 30), "つなぐ"))
+        {
+            Joint();
+        }
+    }
+
+    void Joint()
     {
 
         GameObject _husi_up = new GameObject();
         GameObject _husi_down = new GameObject();
+        _playerA.transform.position = this.transform.position + new Vector3(0, -1.0f, 0);
+        _playerA.transform.rotation = transform.rotation;
+        _playerB.transform.position = this.transform.position + new Vector3(0.01f * (_jointCount + 1), -1, 0);
+        _playerB.transform.rotation = transform.rotation;
 
-        _player1.transform.position = this.transform.position + new Vector3(0, -1, 0);
-        _player2.transform.position = this.transform.position + new Vector3(0.01f * (_jointCount + 1), -1, 0);
-        _player1.transform.rotation = this.transform.rotation;
-        _player2.transform.rotation = this.transform.rotation;
-        
-
-        GameObject _A = _player1;
+        GameObject _A = _playerA;
         GameObject _B;
         Vector3 spawnRotation = new Vector3(0, 0, 0);
 
         for (int i = 0; i < _jointCount + 1; i++)
         {
-            if(i == _jointCount)
-                {
-                _B = _player2;
+            if (i == _jointCount)
+            {
+                _B = _playerB;
             }
             else
                 _B = Instantiate(_joint, this.transform.position + new Vector3(0.01f * i, 0, 0), this.transform.rotation, this.transform);
@@ -59,9 +76,25 @@ public class playerSpawner : MonoBehaviour
             upper.connectedBody = _A.GetComponent<Rigidbody>();
             upper.autoConfigureConnectedAnchor = false;
             upper.anchor = new Vector3(0, +halfHeight, 0);
-            upper.connectedAnchor = new Vector3(0, +halfHeight, 0);
-            // 修正: SoftJointLimit構造体を使ってlinearLimitを設定
-            SoftJointLimit limitStruct = upper.linearLimit;
+
+            if (_B.tag == "Player")
+            {
+                upper.anchor = new Vector3(0, halfHeight * 2.0f + _magicNumber, 0);
+            }
+            else
+            {
+                upper.anchor = new Vector3(0, halfHeight, 0);
+            }
+            if (_A.tag == "Player")
+            {
+                upper.connectedAnchor = new Vector3(0, halfHeight * 2.0f +_magicNumber, 0);
+            }
+            else
+            {
+                upper.connectedAnchor = new Vector3(0, +halfHeight, 0);
+            }
+                // 修正: SoftJointLimit構造体を使ってlinearLimitを設定
+                SoftJointLimit limitStruct = upper.linearLimit;
             limitStruct.limit = 0.1f;
             upper.linearLimit = limitStruct;
             // 修正: 構造体を取得して値を設定し、再代入する
@@ -78,8 +111,22 @@ public class playerSpawner : MonoBehaviour
             lower.zMotion = ConfigurableJointMotion.Limited;
             lower.connectedBody = _A.GetComponent<Rigidbody>();
             lower.autoConfigureConnectedAnchor = false;
-            lower.anchor = new Vector3(0, -halfHeight, 0);
-            lower.connectedAnchor = new Vector3(0, -halfHeight, 0);
+            if (_B.tag == "Player")
+            {
+                lower.anchor = new Vector3(0, _magicNumber, 0);
+            }
+            else
+            {
+                lower.anchor = new Vector3(0, -halfHeight, 0);
+            }
+            if (_A.tag == "Player")
+            {
+                lower.connectedAnchor = new Vector3(0, _magicNumber, 0);
+            }
+            else
+            {
+                lower.connectedAnchor = new Vector3(0, -halfHeight, 0);
+            }
 
             lower.linearLimit = limitStruct;
 
