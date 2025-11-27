21941d78e1836b8eab63378a22deee010ec0de81
Sekiya2084 <slawol284@gmail.com>
2025-11-27 13:09:16 +0900

    -変更 Sekiya内Script,Prefab整理

--- Assets/Sekiya/Script/InGameUI/TeamData.cs
+++ Assets/Sekiya/Script/InGameUI/TeamData.cs
@@ -6,8 +6,6 @@ public class TeamData
     public string teamName;
     public Sprite teamIcon;
 
-    public GoalScore_Network teamGoalScript;
-
     // スコアはRankingManagerが実行時にGoalから取得して設定する
     [HideInInspector] // インスペクタからは非表示でOK
     public float score;
