92ff54a482a5f483407b7b0cb5798ded26661ea0
EnchiIchito <ichitonton@gmail.com>
2025-12-02 17:25:36 +0900

    続き

--- Assets/Enchi/Script/ItemObjectPool.cs
+++ Assets/Enchi/Script/ItemObjectPool.cs
@@ -1,3 +1,4 @@
+using System.Drawing;
 using Unity.Netcode;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -7,6 +8,8 @@ public class ItemObjectPool : NetworkBehaviour
 
     // シングルトンのグローバルなアクセスポイント (public static)
     public static ItemObjectPool Instance { get; private set; }
+
+    [SerializeField] GameObject _itemBomb;
 
     bool[] bools = new bool[(int)ItemVariant.Max];
 
@@ -50,8 +53,42 @@ public class ItemObjectPool : NetworkBehaviour
             if (child.gameObject.GetComponent<ItemBomb>() != null)
             {
                 //生成
+                bools[(int)ItemVariant.Bomb] = true;
+
+                InstatiateObj(_itemBomb, transform, Quaternion.identity, transform).SetActive(false);
             }
 
         }
     }
+
+    private GameObject InstatiateObj(GameObject joint, Transform transform, Quaternion quaternion, Transform pearent)
+    {
+        if (!NetworkManager.Singleton.IsServer)
+        {
+            Debug.Log("[RPC] ClientでRope誤実行されたためスキップ");
+            return null;
+        }
+        ulong clientId = NetworkManager.Singleton.LocalClientId;
+
+        Debug.Log($"[Host] Client {clientId} からRope生成リクエストを受信");
+
+        // Ropeを生成
+        GameObject obj = Instantiate(joint, transform.position, quaternion, pearent);
+        obj.SetActive(false);
+
+        //var netObj = GetComponent<NetworkObject>();
+        //オブジェクトのオーナーを決める
+        //netObj.SpawnWithOwnership(clientId);
+
+        // ClientRpcの送信先を1クライアントに限定
+        ClientRpcParams rpcParams = new ClientRpcParams
+        {
+            Send = new ClientRpcSendParams
+            {
+                TargetClientIds = new ulong[] { clientId } // ← ここで送信先を指定！
+            }
+        };
+        return obj;
+    }
+
 }
